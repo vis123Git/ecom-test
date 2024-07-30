0981b113c6ba08f45b27d857efdd8be647e150dc
VishalKumarSain <vishaldhoria007@gmail.com>
2024-07-30 22:40:59 +0530

    first commit

---
+++ models/product.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+const Product = mongoose.model("product", productSchema);
+module.exports = Product;
