9392ab43946c9a4e92e7c7aebe3663e59b0e84bd
VishalKumarSain <vishaldhoria007@gmail.com>
2024-08-01 22:50:05 +0530

    products

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { signup, login } = require('../controllers/auth.controller');
+const { get_products } = require('../controllers/product.controller');
 const router = express.Router();
 
 /* GET home page. */
@@ -8,5 +9,6 @@ router.get('/', function(req, res, next) {
 });
 router.post("/signup", signup)
 router.post("/login", login)
+router.get("/products", get_products)
 
 module.exports = router;
