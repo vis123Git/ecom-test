1717cbc6f56b775d5db42d7f9807b73ad90fef32
VishalKumarSain <vishaldhoria007@gmail.com>
2024-08-02 22:28:29 +0530

    products

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -14,6 +14,7 @@ const add_new_product = async (req, res) => {
     const product_data = { title, description, price: Number(price), image };
     const add_product = await Product.create(product_data);
     if (!add_product) return res.status(400).json({ status: false, message: "Unable to add product. Please try again later!" });
+    if(add_product.image) add_product.image = `${process.env.BASE_URL}/products/${add_product.image}`
     return res.status(200).json({ status: true, data: add_product, message: "Product added successfully" });
   } catch (error) {
     console.log("error===>", error);
@@ -25,6 +26,9 @@ const get_products = async (req, res) => {
   try {
     const get_product = await Product.find();
     if (!get_product) return res.status(400).json({ status: false, message: "Unable to fetch product. Please try again later!" });
+    get_product.forEach((item)=>{
+      if(item.image) item.image = `${process.env.BASE_URL}/products/${item.image}`
+    })
     return res.status(200).json({ status: true, data: get_product, message: "Product fetch successfully" });
   } catch (error) {
     console.log("error===>", error);
@@ -41,6 +45,7 @@ const get_one_product = async (req, res) => {
 
     const get_product = await Product.findById(id);
     if (!get_product) return res.status(400).json({ status: false, message: "Unable to fetch product. Please try again later!" });
+    if(get_product.image) get_product.image = `${process.env.BASE_URL}/products/${get_product.image}`
     return res.status(200).json({ status: true, data: get_product, message: "Product fetch successfully" });
   } catch (error) {
     console.log("error===>", error);
@@ -67,6 +72,7 @@ const update_one_product = async (req, res) => {
 
     const update_product = await Product.findByIdAndUpdate(id, {$set : data_to_update}, {new:true})
     if (!update_product) return res.status(400).json({ status: false, message: "Unable to update product. Please try again later!" });
+    if(update_product.image) update_product.image = `${process.env.BASE_URL}/products/${update_product.image}`
 
     return res.status(200).json({ status: true, data: update_product, message: "Product updated successfully" });
   } catch (error) {
